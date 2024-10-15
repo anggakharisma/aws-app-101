d9e0442f359882fa359c3f7aa68d13d3e618d4cb
Angga Kharisma <anggakharisma@protonmail.com>
2024-10-15 16:59:56 +0700

    chore: CORS demon

--- backend/server.py
+++ backend/server.py
@@ -15,44 +15,7 @@ db = client['aws_101']
 app = Flask(__name__)
 CORS(app)
 
-@app.post("/login")
-def login():
-    try:
-        content = request.json
-        user = db['users'].find_one({ 
-            'email': content['email']
-        })
 
-        if user == None:
-            return jsonify({
-                "message": 'User or password might be wrong'
-            }), 404
-
-        # incoming password
-        request_password = content['password'].encode('utf-8')
-        request_hashed =  bcrypt.hashpw(request_password, bcrypt.gensalt())
-        result = bcrypt.checkpw(request_password,
-                                user['password'].encode('utf-8'))
-
-        if result == False:
-            return jsonify({
-                "message": 'User or password might be wrong'
-            }), 404
-
-        token = jwt.encode({
-            "email": content['email'],
-        }, os.getenv('JWT_SECRET'), algorithm='HS256')
-
-        return jsonify({
-            'email': content['email'],
-            'token': token
-        }), 200
-    except Exception as e:
-        print(str(e))
-        return jsonify({
-            'message': 'Something went wrong',
-            'err': str(e)
-        }), 500
 
 @app.route("/")
 def index():
@@ -60,5 +23,44 @@ def index():
         'message': 'aws-101-assesment-angga'
     }
 
+
+@app.post("/test")
+def test():
+    return 'test'
+
+@app.post("/login")
+def login():
+    content = request.json
+    user = db['users'].find_one({ 
+        'email': content['email']
+    })
+
+    if user == None:
+        return jsonify({
+            "message": 'User or password might be wrong'
+        }), 404
+
+    # incoming password
+    request_password = content['password'].encode('utf-8')
+    request_hashed =  bcrypt.hashpw(request_password, bcrypt.gensalt())
+    result = bcrypt.checkpw(request_password,
+                            user['password'].encode('utf-8'))
+
+    if result == False:
+        return jsonify({
+            "message": 'User or password might be wrong'
+        }), 404
+
+    token = jwt.encode({
+        "email": content['email'],
+    }, os.getenv('JWT_SECRET'), algorithm='HS256')
+
+    return jsonify({
+        'email': content['email'],
+        'token': token
+    }), 200
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=os.getenv('PORT') if True else 8080, debug=True)
+    app.run(host="0.0.0.0", port=os.getenv('PORT') if True else 8080,
+            debug=False)
